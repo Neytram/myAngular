b9b362e5ab014a2b615cd1bcf3d572d3aa53d3e9
= <=>
2025-01-11 22:31:30 +0100

    Ajout du style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,15 +12,15 @@ import { Post } from './models/post';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  myPostOne!: Post
-  myPostTwo!: Post
-  myPostThree!: Post
+  postList!: Post[]
 
   ngOnInit () {
-    this.myPostOne = new Post("Captain Falcon", "Chasseur de prime", "https://1683a92a.delivery.rocketcdn.me/wp-content/uploads/2020/11/F-Zero-Captain-Falcon-790x450.jpg.webp", new Date(), 4)
-    this.myPostTwo = new Post("Byleth", "Enseignant", "https://image.jeuxvideo.com/medias-crop-1200-675/156346/1563456314-7010-card.jpg", new Date(), 7)
-    this.myPostThree = new Post("Felinferno", "Chat catcheur", "https://www.hindustantimes.com/ht-img/img/2023/05/30/550x309/ezgif-3-13db03efa4_1685470019369_1685470026035.jpg", new Date(), 3)
+    this.postList = [
+      new Post("Captain Falcon", "Chasseur de prime", "https://1683a92a.delivery.rocketcdn.me/wp-content/uploads/2020/11/F-Zero-Captain-Falcon-790x450.jpg.webp", new Date(), 4),
+      new Post("Byleth", "Enseignant", "https://image.jeuxvideo.com/medias-crop-1200-675/156346/1563456314-7010-card.jpg", new Date(), 7),
+      new Post("Felinferno", "Chat catcheur", "https://www.hindustantimes.com/ht-img/img/2023/05/30/550x309/ezgif-3-13db03efa4_1685470019369_1685470026035.jpg", new Date(), 3)
+    ]
 
-    this.myPostOne.setLocation("Mute City")
+    this.postList[0].setLocation("Mute City")
   }
 }
--- src/app/shared-image/shared-image.component.ts
+++ src/app/shared-image/shared-image.component.ts
@@ -1,14 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Post } from '../models/post';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-shared-image',
-  imports: [],
+  imports: [
+    NgStyle
+  ],
   templateUrl: './shared-image.component.html',
   styleUrl: './shared-image.component.scss'
 })
 export class SharedImageComponent implements OnInit {
   @Input() post!: Post
+  @ViewChild("boutonLike") boutonLike!: ElementRef<HTMLButtonElement>
   liked!: boolean
   likeButtonText! : string
 
@@ -24,13 +28,13 @@ export class SharedImageComponent implements OnInit {
       this.post.addLike()
       this.liked = true
       this.likeButtonText = "UnLike"
+      this.boutonLike.nativeElement.className = "boutonLike2"
     } else
     {
       this.post.unLike()
       this.liked = false
       this.likeButtonText = "Like"
+      this.boutonLike.nativeElement.className = "boutonLike"
     }
   }
-
-
 }
